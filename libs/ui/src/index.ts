12ab8d81662009a3c9df43493099a1a5e10c7151
Brad Taniguchi <bradtaniguchi@gmail.com>
2023-03-30 17:59:57 -0700

    chore: add main-page page

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/pages/main-page/main-page';
 export * from './lib/components/base-filters/base-filters';
 export * from './lib/components/repos/repos';
 export * from './lib/components/url/url';
---
+++ libs/ui/src/lib/pages/main-page/main-page.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable-next-line */
+export interface MainPageProps {}
+
+/**
+ * The main-page is the actual UI page routed and managed by nextjs.
+ *
+ * @param props the props of the component
+ */
+export function MainPage(props: MainPageProps) {
+  return (
+    <div>
+      <h1>Welcome to MainPage!</h1>
+    </div>
+  );
+}
+
+export default MainPage;
