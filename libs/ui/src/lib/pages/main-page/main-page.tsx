50c03867b24ef202daf3e13e5cc73b532eb52bb7
Brad Taniguchi <bradtaniguchi@gmail.com>
2023-04-17 15:34:07 -0700

    feat: update responsive layouting

--- libs/ui/src/lib/pages/main-page/main-page.tsx
+++ libs/ui/src/lib/pages/main-page/main-page.tsx
@@ -80,11 +80,11 @@ export function MainPage(props: MainPageProps) {
           }
         />
       </div>
-      <div className="col-span-1 row-span-4">
+      <div className="col-span-5 row-span-4 sm:col-span-1">
         <BaseFilters selected={baseFilters} {...functions} />
       </div>
 
-      <div className="col-span-4 row-span-4">
+      <div className="col-span-5 row-span-4 sm:col-span-4">
         <Repos repos={repos} {...functions} />
       </div>
     </div>
